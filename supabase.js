a2a27277609120876e5d73549c73724da683304a
Parsa23232424 <s.parsa.tahami@gmail.com>
2026-08-06 12:03:51 +0330

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,8 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
 
 // ======================== تنظیمات Supabase ========================
-// اینجا آدرس و کلید پروژه‌ات رو بذار
-const supabaseUrl = 'https://jnmgncodjjilbxynhuyi.supabase.co/rest/v1/';   // <-- Project URL رو عوض کن
-const supabaseKey = 'sb_publishable_aWMTLp8GZDjKSwBN9H65-w_5W8g_G-2';  // <-- 
+const supabaseUrl = 'https://jnmgncodjjilbxynhuyi.supabase.co/rest/v1/';   // <-- Project URL رو بذار
+const supabaseKey = 'sb_publishable_aWMTLp8GZDjKSwBN9H65-w_5W8g_G-2';  // <-- Publishable Key رو بذار
 // ==================================================================
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
