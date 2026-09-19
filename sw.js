d38fcd91e8bf408c316351d5ecb6653c12d69175
Parsa23232424 <s.parsa.tahami@gmail.com>
2026-09-19 17:01:37 +0330

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,70 @@
+
+const CACHE_NAME = "hadaf-pwa-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./login.html",
+  "./signup.html",
+  "./goals.html",
+  "./pico.html",
+  "./calculator.html",
+  "./qr.html",
+  "./manifest.json",
+  "./logo.png",
+  "./logo.ico"
+];
+
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+  );
+
+  self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+
+  event.waitUntil(
+
+    caches.keys().then(keys => {
+
+      return Promise.all(
+
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+
+      );
+
+    })
+
+  );
+
+  self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request)
+      .then(cachedResponse => {
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request);
+
+      })
+
+  );
+
+});
