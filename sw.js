77e7a6bf77abc19d5ada0836daebff425034e3f2
Parsa23232424 <s.parsa.tahami@gmail.com>
2026-09-19 17:10:00 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = "hadaf-pwa-v1";
+const CACHE_NAME = "hadaf-pwa-v2";
 
 const FILES_TO_CACHE = [
   "./",
@@ -12,11 +11,12 @@ const FILES_TO_CACHE = [
   "./qr.html",
   "./manifest.json",
   "./logo.png",
-  "./logo.ico"
+  "./logo.ico",
+  "./logo-192.png",
+  "./logo-512.png"
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -27,44 +27,29 @@ self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-
     caches.keys().then(keys => {
-
       return Promise.all(
-
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-
       );
-
     })
-
   );
 
   self.clients.claim();
 });
 
-
 self.addEventListener("fetch", event => {
-
   event.respondWith(
-
     caches.match(event.request)
       .then(cachedResponse => {
-
         if (cachedResponse) {
           return cachedResponse;
         }
 
         return fetch(event.request);
-
       })
-
   );
-
 });
